bd80beb18afada157b0f20a0b56a91be6636eec2
MarceladSilva <marceladasilvaandrade2009@gmail.com>
2024-05-31 14:02:02 -0300

    Add files via upload

---
+++ Shared/AppState.cs
@@ -0,0 +1,10 @@
+namespace dam_monitoring.Shared;
+
+public static class AppState
+{
+    public const string URL_SIGNALR = "https://dammonitoringapi.bigusclassic.com.br/monitoring";
+    public const string SEND_MONITORING = "SendMonitoring";
+    public const string RECEIVE_MONITORING = "ReceiveMonitoring";
+    
+    public static int Monitoring;
+}
