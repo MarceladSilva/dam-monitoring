9ca205d985c7e60195ae0c16c1dcd7400c3f4307
MarceladSilva <marceladasilvaandrade2009@gmail.com>
2024-06-10 15:38:33 -0300

    Update AppState.cs

--- Shared/AppState.cs
+++ Shared/AppState.cs
@@ -2,7 +2,7 @@ namespace dam_monitoring.Shared;
 
 public static class AppState
 {
-    public const string URL_SIGNALR = "https://dammonitoringapi.bigusclassic.com.br/monitoring";
+    public const string URL_SIGNALR = "http://localhost:5160/monitoring";
     public const string SEND_MONITORING = "SendMonitoring";
     public const string RECEIVE_MONITORING = "ReceiveMonitoring";
     
